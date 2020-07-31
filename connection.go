a6e54f01555d9b885d64c86f80b23d9d50b2996d
Brian Hughes <brian.hughes@dialsource.com>
2020-07-30 23:43:25 -0600

    temporary debug

--- connection.go
+++ connection.go
@@ -209,7 +209,7 @@ func (c *SocketConnection) Handle() {
 	go func() {
 		for {
 			msg, err := newMessage(rbuf, true)
-
+			Debug("received message")
 			if err != nil {
 				c.err <- err
 				done <- true
